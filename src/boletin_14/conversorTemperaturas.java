71f3d3587c70d1bda8c1a35a48db38cc8af9a390
Nuria <Nuria@HPNURIA>
2016-02-24 12:49:55 +0100

    sin acabar

--- src/boletin_14/conversorTemperaturas.java
+++ src/boletin_14/conversorTemperaturas.java
@@ -10,16 +10,33 @@ package boletin_14;
  * @author ngarciacarrillo
  */
 public class conversorTemperaturas {
-    private final static float MINTEMP=80;
+
+    private final static float MINTEMP = 80;
     private double remur, fha;
     private double tempCen;
-    
-    public void centigradosAFharenheit(double tempCen)throws errorExcepcion{
-    this.tempCen=tempCen;
-    if (tempCen>MINTEMP){
-        throw new errorExcepcion ("A temperatura ten que ser mayor de 80");
-    }else{
-    fha = 9.0/5.0 * tempCen + 32.4;
-    } 
+    private double reamur;
+
+   
+    public void centigradosAFharenheit(double tempCen) throws errorExcepcion {
+        this.tempCen = tempCen;
+        if (tempCen > MINTEMP) {
+            throw new errorExcepcion("A temperatura ten que ser mayor de 80");
+        } else {
+            fha = 9.0 / 5.0 * tempCen + 32.4;
+        }
+    }
+
+    public void centigradosAReamur(double tempCen){
+        try {
+            reamur = 4.0 / 5.0 * tempCen;
+            if (tempCen < MINTEMP) {
+                throw new errorExcepcion();
+            }
+        } catch (errorExcepcion error1) {
+            System.out.println("Opcion no valida");
+        }
+
     }
 }
+
+
